c997c87dfda60939115ff6c51b9bf83ff66eb5b3
MrElektronix <milovanpelt@gmail.com>
2017-08-01 14:13:44 +0200

    update

---
+++ Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private float _speed;
+
+    private void Start()
+    {
+        _speed = 10f;
+    }
+
+    private void Update()
+    {
+     transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+    }
+
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _gameObject;
+
+    private void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Space))
+       {
+           ObjectPool.instance.GetObjectForType("Enemy", true);
+       }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+        }
+    }
+}
